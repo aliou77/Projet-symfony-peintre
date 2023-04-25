2c2e9f1ec22a97bb3d8873af86797cc9904ba960
aliou <aliou@gmail.com>
2023-04-26 00:52:25 +0200

    creation du portfolio + integration de lightbox2

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -2,18 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use App\Repository\PeintureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use function PHPUnit\Framework\returnSelf;
+
 class PortfolioController extends AbstractController {
 
     #[Route("/portfolio", name: "portfolio")]
-    public function apropos(): Response{
+    public function apropos(CategoryRepository $repo): Response{
 
         return $this->render("portfolio/portfolio.html.twig", [
             'controllerName' => 'PortfolioController',
-            'current_menu' => 'portfolio'
+            'current_menu' => 'portfolio',
+            'categories' => $repo->findAll()
+        ]);
+    }
+
+    #[Route("/category-{slug}-{id}", name: "portfolio_category", requirements: ['slug' => '[a-z0-9\-]*'])]
+    public function peintures(Category $cat, PeintureRepository $repo): Response {
+
+        return $this->render("portfolio/categories.html.twig", [
+            'peintures' => $repo->findAllPortfolio($cat),
+            'category' => $cat
         ]);
     }
 }
